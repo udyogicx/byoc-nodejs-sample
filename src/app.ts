7cc1aaebfe412cafb8c7708bb9a82c8eb91b4ffc
Binura Gunasekara <binura.g@platformer.com>
2022-07-21 10:10:26 +1000

    init: sample node/ts app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/healthy", (_, res: Response) => {
+  res.send({ status: "heatlhy" });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ msg: "TS App is Running" });
+});
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
